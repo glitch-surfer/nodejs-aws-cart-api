1c170a34c4c4174fddd3f2e7849e998d1407d1e0
vladislav_garaev <vladislav_garaev@epam.com>
2025-10-30 23:41:17 +0100

    init: cdk project

---
+++ infra/test/lib.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-infra';
+// import { Template } from 'aws-cdk-infra/assertions';
+// import * as Lib from '../infra/infra-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in infra/infra-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Lib.LibStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
